2df78cc1b6eb88bf2d7753ff5788aa5c1d84abec
Magnus Hagmar <mhagmar@palantir.com>
2018-09-05 23:27:45 +0200

    Begin instruction implementation and continue on cpu

--- cpu/cpu.py
+++ cpu/cpu.py
@@ -2,21 +2,34 @@ import logging
 
 
 class CPU:
-    def __init__(self):
-        register_pc = 0
-        register_sp = 0xFD
-        register_p = 0x34
-        register_a = 0
-        register_x = 0
-        register_y = 0
+    class Instruction:
+        def __init__(self, function):
+            self.function = function
 
-        memory = [0] * 0xffff
+        def __call__(self):
+            pass
+
+    def __init__(self):
+        self.ZERO = 0x02
+        self.NEGATIVE = 0x80
+
+        self.register_pc = 0
+        self.register_sp = 0xFD
+        self.register_p = 0x34
+        self.register_a = 0
+        self.register_x = 0
+        self.register_y = 0
+
+        self.memory = [0] * 0xffff
+
+        self.opcodes = {
+        }
 
     def p_carry(self):
         return self.register_p & 0x01
 
     def p_zero(self):
-        return self.register_p & 0x02
+        return self.register_p & self.ZERO
 
     def p_interript_disable(self):
         return self.register_p & 0x04
@@ -28,11 +41,4 @@ class CPU:
         return self.register_p & 0x40
 
     def p_negative(self):
-        return self.register_p & 0x80
-
-    class Instruction:
-        def __init__(self, function):
-            self.function = function
-
-        def __call__(self):
-            pass
+        return self.register_p & self.NEGATIVE
--- cpu/instruction.py
+++ cpu/instruction.py
@@ -1,4 +1,8 @@
-from enum import Enum
+def ADC(cpu, addressing_mode):
+    pass
 
 
-class Instruction(Enum):
+def AND(cpu, addressing_mode):
+    operand = addressing_mode.read(cpu)
+    cpu.register_a &= operand
+    cpu.set_status(cpu.ZERO & cpu.NEGATIVE)
