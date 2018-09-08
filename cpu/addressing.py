ed0d07fffadc26b4069617c5090f59d3e14ef9c5
Magnus Hagmar <mhagmar@palantir.com>
2018-09-08 16:51:42 +0200

    Implement writing for addressing mode

--- cpu/addressing.py
+++ cpu/addressing.py
@@ -2,15 +2,32 @@ class ABSOLUTE:
     def read(cpu, param):
         return cpu.memory[param]
 
+    def write(cpu, param, value):
+        cpu.memory[param] = value
+
 
 class ABSOLUTE_X:
     def read(cpu, param):
         return cpu.memory[(param + cpu.x) & 0xFFFF]
 
+    def write(cpu, param, value):
+        cpu.memory[(param + cpu.y) & 0xFFFF] = value
+
 
 class ABSOLUTE_Y:
     def read(cpu, param):
         return cpu.memory[(param + cpu.y) & 0xFFFF]
+
+    def write(cpu, param, value):
+        cpu.memory[(param + cpu.y) & 0xFFFF] = value
+
+
+class ACCUMULATOR:
+    def read(cpu, _):
+        return cpu.a
+
+    def write(cpu, _, value):
+        cpu.a = value
 
 
 class IMMEDIATE:
