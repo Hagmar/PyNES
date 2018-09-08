a194f954cff291e6d77dfbbc6e18e3c2e18e6a42
Magnus Hagmar <mhagmar@palantir.com>
2018-09-08 15:37:09 +0200

    Add absolute, absolute,x and absolute,y addressing

--- cpu/addressing.py
+++ cpu/addressing.py
@@ -1,3 +1,18 @@
+class ABSOLUTE:
+    def read(cpu, param):
+        return cpu.memory[param]
+
+
+class ABSOLUTE_X:
+    def read(cpu, param):
+        return cpu.memory[(param + cpu.x) & 0xFFFF]
+
+
+class ABSOLUTE_Y:
+    def read(cpu, param):
+        return cpu.memory[(param + cpu.y) & 0xFFFF]
+
+
 class IMMEDIATE:
     def read(cpu, param):
         return param
